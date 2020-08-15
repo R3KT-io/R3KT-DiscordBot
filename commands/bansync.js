f46d11d0f2fe70747a7d01f37e16e1ac5b024a68
RetroPronghorn <retropronghorn@gmail.com>
2020-08-15 16:57:30 -0400

    fix(Critical): Fixed bansyncs working in reverse.

--- commands/bansync.js
+++ commands/bansync.js
@@ -1,5 +1,6 @@
 const { language } = require('../../../resources')
 const usernameUtil = require('../../utils/usernames')
+const chalk = require('chalk')
 
 /**
  * Sync bans with another Discord guild
@@ -11,19 +12,28 @@ async function bansync(message, rateLimitQueue, client) {
     const guild = await global.r3kt.discordDBConn.getGuild(guildId)
     const guildBans = await global.r3kt.discordDBConn.getBans(guildId, 'DISCORD')
     const guildUnbans = await global.r3kt.discordDBConn.getUnbans(guildId, 'DISCORD')
+    console.log(`${message.author.username} syncing **${guildBans.length} ban(s)** with guild **${guild.name}**.`)
     rateLimitQueue(() => {
         message.channel.send(`Syncing **${guildBans.length} ban(s)** with guild **${guild.name}**.`)
     })
     guildBans.forEach(ban => {
         rateLimitQueue(() => {
-            const g = client.guilds.cache.get(guildId)
-            g.members.ban(ban.meta.userID, { reason: 'R3KT Ban Sync'})
+            const g = client.guilds.cache.get(message.guild.id)
+            g.members
+                .ban(ban.meta.userID, { reason: 'R3KT Ban Sync'})
+                .catch(err => {
+                    console.log(chalk.yellow(`Failed to ban ${ban.meta.userID}`))
+                })
         })
     })
     guildUnbans.forEach(unban => {
         rateLimitQueue(() => {
             const g = client.guilds.cache.get(guildId)
-            g.members.unban(unban.meta.userID, 'R3KT Unban Sync')
+            g.members
+                .unban(unban.meta.userID, 'R3KT Unban Sync')
+                .catch(err => {
+                    console.log(chalk.yellow(`Failed to unban ${unban.meta.userID}`))
+                })
         })
     })
 }
